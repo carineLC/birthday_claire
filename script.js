560a730e699ad17d8ba203d1ef8e8e5c99260df1
Carine LE CHARLES <carine.le.charles@gmail.com>
2024-06-23 17:51:47 +0200

    added js rule

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function checkInput() {
   const input = document.getElementById('textAnswer').value;
-  if (input.toLowerCase().replace(/\s/g, '').replace(/-/g, '').replace(/é/g, 'e') === 'basiliquedusacrecoeur') {
+  if (input.toLowerCase().replace(/\s/g, '').replace(/-/g, '').replace(/é/g, 'e').replace(/œ/g, 'oe') === 'basiliquedusacrecoeur') {
       window.location.href = 'success.html';
   } else {
       alert('Essaie encore !');
